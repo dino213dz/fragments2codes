0583567e175e6491a6656c2b21bae95911745856
dino213dz <h4ckr213dz@gmail.com>
2019-08-02 01:52:58 +0200

    MAJ barreProgression.sh

--- barreProgression.sh
+++ barreProgression.sh
@@ -8,19 +8,34 @@ function afficherBarre {
 	fb_fait=$1
 	fb_total=$2
 	fb_car_fait="|"
-	fb_car_restant="¤"
-	
+	fb_car_restant="|"
+	fb_col_fait="\033[0m\033[1;41;33m"
+	fb_col_restant="\033[0m\033[0;45;30m"
+	fb_col_texte="\033[0m\033[1;33m"	
 	fb_barre=''
+
+	pourcentage=$(( ($fb_fait*100) / $fb_total ))
+
 	for x in $(seq 1 $fb_total);do
 		fb_barre=$fb_barre""$fb_car_fait""
 	done
 
-	barres_fait=${fb_barre:0:$fb_fait}
+	#
+	txt_fait_sur_total="$fb_fait/$fb_total"	
+	txt_pourcentage="$pourcentage%"
+	texte="$txt_pourcentage-$txt_fait_sur_total"
+	taille_txt=${#texte}
+	pos_txt=$(( ($fb_total-$taille_txt)/2 ))
+	fb_barre_A=${fb_barre:0:$pos_txt}
+	fb_barre_B==${fb_barre_A//$fb_car_fait/$fb_car_restant}
+	fb_barre_texte="$fb_barre_A$texte$fb_barre_B"
+	#
+	barres_fait=${fb_barre_texte:0:$fb_fait}
 	nb_barres_restantes=$(( $fb_total-$fb_fait ))
-	barres_restantes=${fb_barre:0:$nb_barres_restantes}
+	barres_restantes=${fb_barre_texte:$fb_fait:$nb_barres_restantes}
 	barres_restantes=${barres_restantes//$fb_car_fait/$fb_car_restant}
-	pourcentage=$(( ($fb_fait*100) / $fb_total ))
-	barre_progression="\033[1;42;33m"$barres_fait"\033[0m\033[1;32m"$barres_restantes"[$pourcentage%]"
+
+	barre_progression=$fb_col_fait""$barres_fait""$fb_col_restant""$barres_restantes"\033[0m"
 	
 	echo -e $barre_progression
 	}
